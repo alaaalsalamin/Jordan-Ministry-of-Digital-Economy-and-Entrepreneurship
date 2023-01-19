7de1f720e6896dd425a96b141ad3e675940b2f5d
alaaalsalamin <alaaalsalamin8@gmail.com>
2023-01-19 22:19:44 +0300

    3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAMGpLIPo1FniGyf3eUabXc1uB5VzA-5EU",
-  authDomain: "final-m-37486.firebaseapp.com",
-  projectId: "final-m-37486",
-  storageBucket: "final-m-37486.appspot.com",
-  messagingSenderId: "782954633881",
-  appId: "1:782954633881:web:b41775aac1cb52c4c27608",
-  measurementId: "G-2VKZH4GWB3"
+    apiKey: "AIzaSyBrtngvOmZE2O4AQxixFPk6x-87e5om_PA",
+    authDomain: "ministry-of-digital-econ-a4f2d.firebaseapp.com",
+    projectId: "ministry-of-digital-econ-a4f2d",
+    storageBucket: "ministry-of-digital-econ-a4f2d.appspot.com",
+    messagingSenderId: "483274507350",
+    appId: "1:483274507350:web:0a1dd96b6e26181a9127f7",
+    measurementId: "G-8X5MC63BST"
   }
 };
 
